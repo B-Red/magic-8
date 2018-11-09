5c558960202c27414108128dca462b1f501594f3
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-09 14:18:25 -0700

    random answer returned

--- fp_backend/index.js
+++ fp_backend/index.js
@@ -13,6 +13,11 @@ app.get('/answers', (req, res, next) => {
     res.send({ data: data })
 })
 
+app.get('/return', (req, res) => {
+    let i = Math.floor(Math.random() * (data.length + 1))
+    res.send({data: data[i].answer})
+})
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
