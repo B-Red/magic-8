2535faa95c85bceb7f73e12a68cc66c717100ab8
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-12 06:55:03 +0100

    trying to get data to display

--- fp_client/magic-8-ball/src/App.js
+++ fp_client/magic-8-ball/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AnswerDisplay from './Components/AnswerDisplay';
 
 class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        randomAnswer : null,
+    }
+  }
+  
+  async componentDidMount() {
+    fetch('http://localhost:3001/return')
+      .then(response => response.json())
+      .then(randomAnswer => this.setState({ randomAnswer }));
+  }
+  
+  
+  
+  
   render() {
+    console.log(this.state.randomAnswer)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <AnswerDisplay randomAnswer={this.state.randomAnswer} />
+        <div></div>
       </div>
     );
   }
---
+++ fp_client/magic-8-ball/src/Components/AnswerDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnswerDisplay = (props) => {
+    return(
+        <div>fuckkkckckckc</div>
+    )
+}
+
+export default AnswerDisplay
